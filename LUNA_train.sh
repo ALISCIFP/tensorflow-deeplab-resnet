2e31a098590a5b7d056fab758dcb3db86adbb7ee
Zengming Shen <szm0219@gmail.com>
2017-05-30 11:35:59 -0500

    LUNA16 train script

---
+++ LUNA_train.sh
@@ -0,0 +1,9 @@
+python train.py  \
+--data-dir /home/zack/Data/LUNA16/  \
+--data-list /home/zack/Data/LUNA16/dataset/train.txt \
+--num-classes 5 \
+--not-restore-last \
+--input-size '384,384'  \
+--snapshot-dir './snapshots_LUNA16/' \
+
+
