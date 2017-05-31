838f17c932195d247afc3355561134ee594d0fcc
Zengming Shen <szm0219@gmail.com>
2017-05-30 23:55:05 -0500

    computer images_jpg mean

---
+++ compute_image_mean.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+# This script belongs to https://github.com/
+
+import os,glob
+import argparse
+
+import numpy as np
+
+from scipy import misc
+
+
+DATA_DIRECTORY = '/home/zack/Data/ILD_jpg_png/'
+DATA_LIST_PATH = '/home/zack/Data/ILD_jpg_png/dataset/train.txt'
+
+DATA_DIRECTORY = '/home/zack/Data/LUNA16/'
+DATA_LIST_PATH = '/home/zack/Data/LUNA16/dataset/train.txt'
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser = argparse.ArgumentParser(description="mdh to jpg-png file converter")
+
+    parser.add_argument("--data-dir", type=str, default=DATA_DIRECTORY,
+                        help="Path to the directory containing the ILD dataset.")
+    parser.add_argument("--data-list", type=str, default=DATA_LIST_PATH,
+                        help="Path to the directory containing the ILD dataset in jpg and png format.")
+
+
+    args = parser.parse_args()
+    flist = open(args.data_list)
+    count = 0
+    im_mean =[0.0,0.0,0.0]
+    for f in flist:
+        fjpg= args.data_dir+f.split()[0]
+        jpgnparray = misc.imread(fjpg)
+        im_mean += np.mean(np.mean(jpgnparray,axis=0),axis =0)
+        count+=1
+        if count % 1000 == 0:
+            print count
+
+    print im_mean/count
+
+
+
+
+
+if __name__ == '__main__':
+    main()
