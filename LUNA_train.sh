f8e4e0e6c811d7580d653679ddd58f5b3646c508
Zengming Shen <szm0219@gmail.com>
2017-05-31 02:24:12 -0500

    add image mean

--- LUNA_train.sh
+++ LUNA_train.sh
@@ -1,9 +1,10 @@
 python train.py  \
 --data-dir /home/zack/Data/LUNA16/  \
 --data-list /home/zack/Data/LUNA16/dataset/train.txt \
+--batch-size 5 \
 --num-classes 5 \
 --not-restore-last \
---input-size '384,384'  \
---snapshot-dir './snapshots_LUNA16/' \
-
-
+--input-size '512,512'  \
+--snapshot-dir './snapshots_LUNA16_all8_400k/' \
+--gpu-mask '1'  \
+--num-steps 400000
--- deeplab_resnet/utils.py
+++ deeplab_resnet/utils.py
@@ -3,16 +3,28 @@ import numpy as np
 import tensorflow as tf
 
 # colour map
+# label_colours = [(0,0,0)
+#                 # 0=background
+#                 ,(128,0,0),(0,128,0),(128,128,0),(0,0,128),(128,0,128)
+#                 # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
+#                 ,(0,128,128),(128,128,128),(64,0,0),(192,0,0),(64,128,0)
+#                 # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
+#                 ,(192,128,0),(64,0,128),(192,0,128),(64,128,128),(192,128,128)
+#                 # 11=diningtable, 12=dog, 13=horse, 14=motorbike, 15=person
+#                 ,(0,64,0),(128,64,0),(0,192,0),(128,192,0),(0,64,128)]
+#                 # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
+
+#for LUNA 16
+# label_colours = [(0,0,0)
+#                 # 0=background
+#                 ,(128,0,0),(0,128,0),(128,128,0),(0,0,128)]
+#                 # 1=left lung, 2=right lung, 3=pipe, 4=nodule,
+
+#for ILD
 label_colours = [(0,0,0)
                 # 0=background
-                ,(128,0,0),(0,128,0),(128,128,0),(0,0,128),(128,0,128)
-                # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
-                ,(0,128,128),(128,128,128),(64,0,0),(192,0,0),(64,128,0)
-                # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
-                ,(192,128,0),(64,0,128),(192,0,128),(64,128,128),(192,128,128)
-                # 11=diningtable, 12=dog, 13=horse, 14=motorbike, 15=person
-                ,(0,64,0),(128,64,0),(0,192,0),(128,192,0),(0,64,128)]
-                # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
+                ,(128,128,0),(0,0,128)]
+                # 1= lungs, 2=nodules,
 
 def decode_labels(mask, num_images=1, num_classes=21):
     """Decode batch of segmentation masks.
