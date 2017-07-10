5dea256153b54b9cab75d14b0a7e20333f5d81e5
Zengming Shen <szm0219@gmail.com>
2017-07-09 20:41:44 -0500

    LITS

---
+++ LITS_resume.sh
@@ -0,0 +1,12 @@
+python train.py  \
+--data-dir /home/zack/Data/LITS/ \
+--data-list  /home/zack/Data/LITS/dataset/train.txt  \
+--val-data-list /home/zack/Data/LITS/dataset/val.txt  \
+--batch-size 4 \
+--num-classes 3 \
+--input-size '512,512'  \
+--is-training \
+--restore-from './snapshots/LITS4' \
+--snapshot-dir './snapshots/LITS4t2' \
+--gpu-mask '1'  \
+--num-steps 150000 \
