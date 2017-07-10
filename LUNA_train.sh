e56e64e1905dd2763fd65583b5785ccd5ec7cdc1
Zengming Shen <szm0219@gmail.com>
2017-07-09 20:01:57 -0500

    success for LiTS

--- LUNA_train.sh
+++ LUNA_train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 python train.py  \
---data-dir /home/victor/LUNA16 \
---data-list /mnt/data/LUNA16/dataset/train.txt \
---val-data-list /mnt/data/LUNA16/dataset/val.txt \
+--data-dir /home/zack/Data/LUNA16 \
+--data-list /home/zack/Data/LUNA16/dataset/train.txt \
+--val-data-list /home/zack/Data/LUNA16/dataset/val.txt \
 --not-restore-last
