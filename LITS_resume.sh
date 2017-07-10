7167e9f8b61b63e7edd7896fb0ad68ca92083d6c
Zengming Shen <szm0219@gmail.com>
2017-07-09 21:36:33 -0500

    refine success on LITS

--- LITS_resume.sh
+++ LITS_resume.sh
@@ -3,9 +3,10 @@ python train.py  \
 --data-list  /home/zack/Data/LITS/dataset/train.txt  \
 --val-data-list /home/zack/Data/LITS/dataset/val.txt  \
 --batch-size 4 \
+--not-restore-last \
 --num-classes 3 \
 --input-size '512,512'  \
 --restore-from './snapshots/LITS4t2' \
 --snapshot-dir './snapshots/LITS4t2_refine' \
 --gpu-mask '1'  \
---num-steps 290045 \
+--num-steps 435068 \
