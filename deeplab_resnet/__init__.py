e03edbd15fb46a8a2ade374df5a21639a1f1dc31
treewarp <treewarp@no.reply.users.github.com>
2017-07-14 19:21:24 -0500

    delete old model

--- deeplab_resnet/__init__.py
+++ deeplab_resnet/__init__.py
@@ -1,5 +1,4 @@
 from .LUNA16image_reader import ImageReader_LUNA16
 from .image_reader import ImageReader
 from .model import DeepLabResNetModel
-from .model import DeepLabResNetModelOld
 from .utils import decode_labels, inv_preprocess, prepare_label
