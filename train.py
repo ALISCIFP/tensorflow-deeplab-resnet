fc856d5dc4d1a5c70af664c45afcd77ec0b7821f
treewarp <treewarp@no.reply.users.github.com>
2017-07-20 22:14:14 -0500

    DPN

--- train.py
+++ train.py
@@ -23,12 +23,13 @@ from deeplab_resnet import DeepLabResNetModel, ImageReader, decode_labels, inv_p
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
 
 # IMG_MEAN = np.array((104.00698793, 116.66876762, 122.67891434), dtype=np.float32)  # VOC2012
-IMG_MEAN = np.array((40.9729668, 42.62135134, 40.93294311), dtype=np.float32)  # ILD
+# IMG_MEAN = np.array((40.9729668, 42.62135134, 40.93294311), dtype=np.float32)  # ILD
 #IMG_MEAN = np.array((88.89328702, 89.36887475, 88.8973059), dtype=np.float32)  # LUNA16
 # IMG_MEAN = np.array((109.5388, 118.6897, 124.6901), dtype=np.float32)  # ImageNet2016 Scene-parsing Mean
 #
 # LUNA16_softmax_weights = np.array((2.15129033634559E-05, 0.0002845522, 0.0002506645, 0.0123730652, 0.9870702051),
 #                                   dtype=np.float32)
+IMG_MEAN = np.array((46.02499091, 46.00602707, 45.95747361), dtype=np.float32)  #LITS
 
 GPU_MASK = '1'
 BATCH_SIZE = 6
