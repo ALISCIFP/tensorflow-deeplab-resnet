e0ae410c7a520190b70038cc5c69aa547f43bb1c
Zengming Shen <szm0219@gmail.com>
2017-07-22 19:56:21 -0500

    mm

--- deeplab_resnet/image_reader.py
+++ deeplab_resnet/image_reader.py
@@ -109,7 +109,7 @@ def read_images_from_disk(input_queue, input_size, random_scale, random_mirror, 
     img_contents = tf.read_file(input_queue[0])
     label_contents = tf.read_file(input_queue[1])
     
-    img = tf.image.decode_png(img_contents, channels=3)
+    img = tf.image.decode_jpeg(img_contents, channels=3)
     img_r, img_g, img_b = tf.split(value=img, num_or_size_splits=3, axis=2)
     img = tf.cast(tf.concat(axis=2, values=[img_b, img_g, img_r]), dtype=tf.float32)
     # Extract mean.
@@ -119,13 +119,11 @@ def read_images_from_disk(input_queue, input_size, random_scale, random_mirror, 
 
     if input_size is not None:
         h, w = input_size
-        random_scale = False
         # Randomly scale the images and labels.
         if random_scale:
             img, label = image_scaling(img, label)
 
         # Randomly mirror the images and labels.
-        random_mirror = False
         if random_mirror:
             img, label = image_mirroring(img, label)
 
