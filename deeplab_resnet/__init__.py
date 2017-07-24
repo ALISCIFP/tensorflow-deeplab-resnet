255c53c0f3495037ca7f3773d3d5f6bb4761fa2e
Zengming(Zack) Shen <szm0219@gmail.com>
2017-07-24 10:31:31 -0400

    pass2 on s and LUNA 16 removed

--- deeplab_resnet/__init__.py
+++ deeplab_resnet/__init__.py
@@ -1,4 +1,3 @@
-from .LUNA16image_reader import ImageReader_LUNA16
 from .image_reader import ImageReader
 from .model import DeepLabResNetModel
 from .utils import decode_labels, inv_preprocess, prepare_label
