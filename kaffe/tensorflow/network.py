227eea94bc98c6600f59a4fe9d96e360e21c1e80
Zengming Shen <szm0219@gmail.com>
2017-07-23 21:39:22 -0400

    selu-dropout bug fix

--- kaffe/tensorflow/network.py
+++ kaffe/tensorflow/network.py
@@ -40,7 +40,7 @@ def layer(op):
 
 class Network(object):
 
-    def __init__(self, inputs, trainable=True, is_training=False, num_classes=21):
+    def __init__(self, inputs,trainable=True, is_training=False, num_classes=21):
         # The input nodes for this network
         self.inputs = inputs
         # The current list of terminal nodes
@@ -206,43 +206,43 @@ class Network(object):
         return scale * tf.where(input >= 0.0, input, alpha * tf.nn.elu(input,name))
 
     @layer
-    def dropout_selu(self,x, rate, alpha=-1.7580993408473766, fixedPointMean=0.0, fixedPointVar=1.0,
+    def dropout_selu(self,input, rate, alpha=-1.7580993408473766, fixedPointMean=0.0, fixedPointVar=1.0,
                      noise_shape=None, seed=None, name=None, training=False):
         """Dropout to a value with rescaling."""
 
-        def dropout_selu_impl(x, rate, alpha, noise_shape, seed, name):
+        def dropout_selu_impl(input, rate, alpha, noise_shape, seed, name):
             keep_prob = 1.0 - rate
-            x = ops.convert_to_tensor(x, name="x")
+            input = ops.convert_to_tensor(input, name="input")
             if isinstance(keep_prob, numbers.Real) and not 0 < keep_prob <= 1:
                 raise ValueError("keep_prob must be a scalar tensor or a float in the "
                                  "range (0, 1], got %g" % keep_prob)
-            keep_prob = ops.convert_to_tensor(keep_prob, dtype=x.dtype, name="keep_prob")
+            keep_prob = ops.convert_to_tensor(keep_prob, dtype=input.dtype, name="keep_prob")
             keep_prob.get_shape().assert_is_compatible_with(tensor_shape.scalar())
 
-            alpha = ops.convert_to_tensor(alpha, dtype=x.dtype, name="alpha")
+            alpha = ops.convert_to_tensor(alpha, dtype=input.dtype, name="alpha")
             alpha.get_shape().assert_is_compatible_with(tensor_shape.scalar())
 
             if tensor_util.constant_value(keep_prob) == 1:
-                return x
+                return input
 
-            noise_shape = noise_shape if noise_shape is not None else array_ops.shape(x)
+            noise_shape = noise_shape if noise_shape is not None else array_ops.shape(input)
             random_tensor = keep_prob
-            random_tensor += random_ops.random_uniform(noise_shape, seed=seed, dtype=x.dtype)
+            random_tensor += random_ops.random_uniform(noise_shape, seed=seed, dtype=input.dtype)
             binary_tensor = math_ops.floor(random_tensor)
-            ret = x * binary_tensor + alpha * (1 - binary_tensor)
+            ret = input * binary_tensor + alpha * (1 - binary_tensor)
 
             a = math_ops.sqrt(fixedPointVar / (
             keep_prob * ((1 - keep_prob) * math_ops.pow(alpha - fixedPointMean, 2) + fixedPointVar)))
 
             b = fixedPointMean - a * (keep_prob * fixedPointMean + (1 - keep_prob) * alpha)
             ret = a * ret + b
-            ret.set_shape(x.get_shape())
+            ret.set_shape(input.get_shape())
             return ret
 
-        with ops.name_scope(name, "dropout", [x]) as name:
+        with ops.name_scope(name, "dropout", [input]) as name:
             return utils.smart_cond(training,
-                                    lambda: dropout_selu_impl(x, rate, alpha, noise_shape, seed, name),
-                                    lambda: array_ops.identity(x))
+                                    lambda: dropout_selu_impl(input, rate, alpha, noise_shape, seed, name),
+                                    lambda: array_ops.identity(input))
 
     @layer
     def max_pool(self, input, k_h, k_w, s_h, s_w, name, padding=DEFAULT_PADDING):
