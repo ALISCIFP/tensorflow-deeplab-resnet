6fcaa7141efe34848dfa119e145977a8c336d7f7
Zengming(Zack) Shen <szm0219@gmail.com>
2017-07-24 14:36:53 -0400

    rm LUNA reader

--- deeplab_resnet/__init__.py
+++ deeplab_resnet/__init__.py
@@ -1,4 +1,3 @@
 from .model import DeepLabResNetModel
 from .image_reader import ImageReader
 from .utils import decode_labels, inv_preprocess, prepare_label
-from .LUNA16image_reader import ImageReader_LUNA16
