abcea533720d28565eee911f0488982fc98e6377
treewarp <treewarp@no.reply.users.github.com>
2017-08-04 04:35:55 -0500

    old model

--- convert_mhd2jpg_png_LiTS_reduced_blanks_new.py
+++ convert_mhd2jpg_png_LiTS_reduced_blanks_new.py
@@ -10,7 +10,6 @@ import multiprocessing
 import os
 
 import SimpleITK as sitk
-import cv2
 import numpy as np
 import scipy.misc
 import scipy.ndimage
@@ -51,18 +50,12 @@ def rescale(input_image, output_spacing, bilinear=False, input_spacing=None, out
 
 
 def ndarry2jpg_png((data_file, img_gt_file, out_dir)):
-    ftrain = []
-    fval = []
-    ftrain_1mm = []
+    ftest = []
     img = sitk.ReadImage(data_file)
     img_gt = sitk.ReadImage(img_gt_file)
 
     spacing = img.GetSpacing()
     gt_spacing = img_gt.GetSpacing()
-    if any([x == 1.0 for x in spacing[0:2]]):
-        print(data_file, img_gt_file, spacing, 'fail!')
-    else:
-        print data_file, img_gt_file, spacing
 
     img, input_spacing, out_size = rescale(img, output_spacing=[0.6, 0.6, 0.7], bilinear=True)
     img_gt, _, _ = rescale(img_gt, output_spacing=[0.6, 0.6, 0.7], bilinear=False, input_spacing=input_spacing,
@@ -72,7 +65,6 @@ def ndarry2jpg_png((data_file, img_gt_file, out_dir)):
     num_slices = img.shape[2]
     img_gt = sitk.GetArrayFromImage(img_gt).transpose()
     data_path, fn = os.path.split(data_file)
-    data_path, fn_gt = os.path.split(img_gt_file)
 
     img_pad = np.pad(img, ((0, 0), (0, 0), (1, 1)), 'constant', constant_values=(0, 0))
 
@@ -99,16 +91,10 @@ def ndarry2jpg_png((data_file, img_gt_file, out_dir)):
 
         img3c = img_pad[:, :, i:i + 3]
         scipy.misc.imsave(os.path.join(out_dir, "JPEGImages", fn + "_" + str(i) + ".jpg"), img3c)
-        cv2.imwrite(os.path.join(out_dir, "PNGImages", fn_gt + "_" + str(i) + ".png"), img_gt[:, :, i])
-        out_string = "/JPEGImages/" + fn + "_" + str(i) + ".jpg\t" + "/PNGImages/" + fn_gt + "_" + str(i) + ".png\n"
-        if any([x == 1.0 for x in spacing[0:2]]):
-            ftrain_1mm.append(out_string)
-        elif '99' in data_file:
-            fval.append(out_string)
-        else:
-            ftrain.append(out_string)
+        out_string = "/JPEGImages/" + fn + "_" + str(i) + ".jpg\n"
+        ftest.append(out_string)
 
-    return (ftrain, fval, ftrain_1mm)
+    return ftest
 
 
 def convert(data_dir, out_dir):
@@ -120,8 +106,6 @@ def convert(data_dir, out_dir):
     print "converting"
     if not os.path.exists(os.path.join(out_dir, "JPEGImages")):
         os.mkdir(os.path.join(out_dir, "JPEGImages"))
-    if not os.path.exists(os.path.join(out_dir, "PNGImages")):
-        os.mkdir(os.path.join(out_dir, "PNGImages"))
     if not os.path.exists(os.path.join(out_dir, "dataset")):
         os.mkdir(os.path.join(out_dir, "dataset"))
 
@@ -129,16 +113,10 @@ def convert(data_dir, out_dir):
     retval = p.map(ndarry2jpg_png, zip(vols, segs, itertools.repeat(out_dir, len(vols))))
     p.close()
 
-    list_train = list(itertools.chain.from_iterable([sublist[0] for sublist in retval]))
-    list_val = list(itertools.chain.from_iterable([sublist[1] for sublist in retval]))
-    list_train_1mm = list(itertools.chain.from_iterable([sublist[2] for sublist in retval]))
+    list_test = list(itertools.chain.from_iterable([sublist[0] for sublist in retval]))
 
-    with open(os.path.join(out_dir, "dataset/train.txt"), 'w') as ftrain:
-        ftrain.writelines(list_train)
-    with open(os.path.join(out_dir, "dataset/train1mm.txt"), 'w') as ftrain_1mm:
-        ftrain_1mm.writelines(list_train_1mm)
-    with open(os.path.join(out_dir, "dataset/val.txt"), 'w') as fval:
-        fval.writelines(list_val)
+    with open(os.path.join(out_dir, "dataset/test.txt"), 'w') as ftest:
+        ftest.writelines(list_test)
 
     print "done."
 
