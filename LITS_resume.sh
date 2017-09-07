c61cf6b1b06bf1b8e1eb100da8e3c9c76f7e3180
zack <szm0219@gmail.com>
2017-09-06 22:56:50 -0500

    for 667 resample

--- LITS_resume.sh
+++ LITS_resume.sh
@@ -1,14 +1,14 @@
 python train.py  \
---data-dir /home/zack/Data/LITS \
---data-list  /home/zack/Data/LITS/dataset/train.txt  \
---val-data-list /home/zack/Data/LITS/dataset/val.txt  \
+--data-dir /home/zack/Data/LITS_resample_667mm \
+--data-list  /home/zack/Data/LITS_resample_667mm/dataset/train.txt  \
+--val-data-list /home/zack/Data/LITS_resample_667mm/dataset/val.txt  \
 --batch-size 5 \
 --num-classes 3 \
 --input-size '512,512'  \
---restore-from './snapshots/LITS4_mm' \
---snapshot-dir './snapshots/LITS4-rmm' \
+--restore-from './deeplab_resnet.ckpt' \
+--snapshot-dir './snapshots/LITS667mm' \
 --gpu-mask '0' \
---learning-rate 2.0e-4 \
+--learning-rate 2.5e-4 \
 --random-scale \
 --random-mirror \
---num-steps 300000 \
+--num-steps 1691820 \ # 20epoch
