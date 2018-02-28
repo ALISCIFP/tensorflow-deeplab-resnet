bda1a2427a25089037c8275c1b84d19a43bfb79b
Zack Shen <szm0219@gmail.com>
2018-02-27 21:41:29 -0600

    input to 512

--- LITS_train.sh
+++ LITS_train.sh
@@ -1,10 +1,11 @@
 python train.py  \
 --data-dir /home/zack/Data/paperrescaledLITS/ \
---data-list /home/zack/Data/paperrescaledLITS/dataset/train.txt \
---val-data-list /home/zack/Data/paperrescaledLITS/dataset/val.txt \
---snapshot-dir './snapshots/HanResNet9Slices' \
+--data-list /home/zack/Data/LITS/dataset/train.txt \
+--val-data-list /home/zack/Data/LITS/dataset/val.txt \
+--snapshot-dir './snapshots/HanResNet5Slices' \
 --gpu-mask '0,1' \
 --learning-rate 1e-3 \
+--input-size '512,512' \
 --random-scale \
 --random-mirror \
 --num-steps 237910 \
