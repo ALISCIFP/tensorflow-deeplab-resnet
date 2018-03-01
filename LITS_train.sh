d9cd9eb0fba3dac581dcfae6a0482e7c1fedb2f6
Zack <szm0219@gmail.com>
2018-02-28 19:57:54 -0600

    no file output for HanresNet 320 train

--- LITS_train.sh
+++ LITS_train.sh
@@ -2,11 +2,11 @@ python train.py  \
 --data-dir /home/zack/Data/paperLITS \
 --data-list /home/zack/Data/paperLITS/dataset/train.txt \
 --val-data-list /home/zack/Data/paperLITS/dataset/val.txt \
---snapshot-dir './snapshots/HanResNet5Slices' \
+--snapshot-dir './snapshots/HanResNet5Slices_320' \
 --gpu-mask '0,1' \
 --batch-size 5 \
---learning-rate 1e-3 \
+--learning-rate 2.5e-4 \
 --input-size '320,320' \
 --random-scale \
 --random-mirror \
---num-steps 237910 \
+--num-steps 118955 \
