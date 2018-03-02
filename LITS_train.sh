b414030d5454bf1bb8a89a46370a6d35c2b4fea4
Zack <szm0219@gmail.com>
2018-03-02 12:44:20 -0600

    test

--- LITS_train.sh
+++ LITS_train.sh
@@ -1,9 +1,9 @@
 python train.py  \
---data-dir /home/zack/Data/paperrescaledLITS/ \
---data-list /home/zack/Data/paperrescaledLITS/dataset/train.txt \
---val-data-list /home/zack/Data/paperrescaledLITS/dataset/val.txt \
+--data-dir /home/zack/Data/paperLITS/ \
+--data-list /home/zack/Data/paperLITS/dataset/train.txt \
+--val-data-list /home/zack/Data/paperLITS/dataset/val.txt \
 --snapshot-dir './snapshots/CDenseUNet' \
---learning-rate 1e-3 \
+--learning-rate 2.5e-4 \
 --random-scale \
 --random-mirror \
 --num-steps 237910 \
