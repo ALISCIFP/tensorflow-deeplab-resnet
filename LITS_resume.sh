e04fd8ceb6eb6dbafcc5e700d48183760a5f7747
Zack <szm0219@gmail.com>
2018-03-02 11:33:56 -0600

    100epoch

--- LITS_resume.sh
+++ LITS_resume.sh
@@ -1,11 +1,14 @@
 python train.py  \
---data-dir /home/zack/Data/paperrescaledLITS/ \
---data-list /home/zack/Data/paperrescaledLITS/dataset/train.txt \
---val-data-list /home/zack/Data/paperrescaledLITS/dataset/val.txt \
---restore-from './snapshots/HanResNet' \
---snapshot-dir './snapshots/HanResNet9Slices' \
+--data-dir /home/zack/Data/paperLITS/ \
+--data-list /home/zack/Data/paperLITS/dataset/train.txt \
+--val-data-list /home/zack/Data/paperLITS/dataset/val.txt \
+--restore-from './snapshots/HanResNet5Slices_320_R60k' \
+--snapshot-dir './snapshots/HanResNet5Slices_320_R120k' \
 --gpu-mask '0,1' \
---learning-rate 1e-3 \
+--batch-size 5 \
+--learning-rate 2.5e-4 \
+--input-size '320,320' \
 --random-scale \
 --random-mirror \
---num-steps 237910 \
+--num-steps 240000 \
+
