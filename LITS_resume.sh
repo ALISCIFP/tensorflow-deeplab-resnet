829a48dd3880b7c059eb19983e0cbbe49a9cd86f
Zack <szm0219@gmail.com>
2018-03-30 03:01:32 -0500

    stopped

--- LITS_resume.sh
+++ LITS_resume.sh
@@ -2,13 +2,13 @@ python train.py  \
 --data-dir /home/zack/Data/paperLITS/ \
 --data-list /home/zack/Data/paperLITS/dataset/train.txt \
 --val-data-list /home/zack/Data/paperLITS/dataset/val.txt \
---restore-from './snapshots/HanResNet5Slices_320_R480k' \
---snapshot-dir './snapshots/HanResNet5Slices_320_R960k' \
---gpu-mask '0' \
+--restore-from './snapshots/HanResNet5Slices_320_R960k' \
+--snapshot-dir './snapshots/HanResNet5Slices_ReLu_320_R1920k' \
+--gpu-mask '0,1' \
 --batch-size 5 \
 --learning-rate 2.5e-4 \
 --input-size '320,320' \
 --random-scale \
 --random-mirror \
---num-steps 1920000 \
+--num-steps 3840000 \
 
